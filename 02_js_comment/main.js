42e89504372d86a8e4fb5e8ec5977ee2b2e81f27
Rasel Rana Earshad <charry.br10@gmail.com>
2024-07-12 02:30:47 +0600

    02 Js Comments

---
+++ 02_js_comment/main.js
@@ -0,0 +1,14 @@
+// Single line comments start with "//".
+
+/* Multi-line comments start with /* and end with */
+
+// Any text between /* and */ will be ignored by JavaScript.
+
+/* There are two types of comment in JavaScript.
+
+    1. Single line Comment
+    2. Block Comment.
+
+ It is most common to use single line comments.
+ Block comments are often used for formal documentation.
+*/
