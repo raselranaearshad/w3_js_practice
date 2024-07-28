21d6026befc06c551ddbfb36f5c48bb9ffe0a367
Rasel Rana Earshad <charry.br10@gmail.com>
2024-07-28 22:10:17 +0600

    Added new notes inside js operators folder

---
+++ 04_js_operators/4.2_js_assignment_operators/README.md
@@ -0,0 +1,30 @@
+# JavaScript Assignment Operator
+Assignment operators assign values to JavaScript Variables.
+|Operator|Example|Same As|
+|--------|-------|-------|
+|=|x = y|x = y|
+|+=|x += y|x = x + y|
+|-=|x -= y|x = x - y|
+|*=|x *= y|x = x * y|
+|/=|x /= y|x = x / y|
+|%=|x %= y|x = x % y|
+|**=|x **= y|x = x ** y|
+
+## Shift Assignment Operator
+|Operator|Example|Same As|
+|--------|-------|-------|
+|<<=|x <<= y|x = x << y|
+|>>=|x >>= y|x = x >> y|
+|>>>=|x >>>= y|x = x >>>= y|
+
+## Bitwise Assignment Operator
+|Operator|Example|Same As|
+|--------|-------|-------|
+|&=|x &= y|x = x & y|
+|^=|x ^= y|x = x ^ y|
+
+## Bitwise Assignment Operator
+|Operator|Example|Same As|
+|--------|-------|-------|
+|&&=|x &&= y|x = x && (x = y)|
+|??=|x ??= y|x = x ?? (x = y)|
---
+++ 04_js_operators/4.2_js_assignment_operators/main.js
@@ -0,0 +1,60 @@
+// Addition Assignment Operator:
+let text = 'Hello';
+text += 'World';
+document.write(text + "<br>");
+
+// Substraction Assignment Operator:
+let x = 10;
+x -= 5;
+document.write(x + "<br>");
+
+// Multiplication Assignment Operator:
+let X = 11;
+X *= 3;
+document.write(X + "<br>")
+
+// Exponentiation Assignment Operator:
+let y = 3;
+y **= 3;
+document.write(y + "<br>")
+
+// Division Assignment Operator:
+let Y = 100;
+Y /= 5;
+document.write(Y + "<br>");
+
+// Remainder Assignment Operator:
+let a = 50;
+a %= 5;
+document.write(a + "<br>");
+
+// Left Shift Assignment Operator:
+let b = -100;
+b <<= 4;
+document.write(b + "<br>");
+
+// Right Shift Assignment Operator:
+let B = -100;
+B >>= 5;
+document.write(B + "<br>");
+
+// Unsigned Right Shift Assignment Operator:
+let U = 100;
+U >>>= 5;
+document.write(U + "<br>")
+
+// Bitwise AND Assignment Operator:
+let c = 10;
+c &= 5;
+/* 
+The Bitwise AND Assignment Operator does a bitwise AND operation on two operands and assigns the result to the the variable.
+*/
+document.write(c + "<br>");
+
+// Bitwise OR Assignment Operator:
+let C = 100;
+C |= 5;
+/* 
+The Bitwise OR Assignment Operator does a bitwise OR operation on two operands and assigns the result to the variable.
+*/
+document.write(C + "<br>")
