71d06941a7f5a7d4c532b4a8605d5646b6bbe0af
Rasel Rana Earshad <charry.br10@gmail.com>
2024-07-28 20:48:16 +0600

    Js Operators

--- 04_js_operators/4.1_js_arithmetic_operators/main.js
+++ 04_js_operators/4.1_js_arithmetic_operators/main.js
@@ -8,4 +8,39 @@ document.write(z + "<br>");
 let X = 100;
 let Y = 36;
 let Z = X - Y;
-document.write(Z);
+document.write(Z + "<br>");
+
+// Multiplication Operator:
+let a = 100;
+let b = 10;
+let c = a * b;
+document.write(c + "<br>");
+
+// Division Operator:
+let A = 1000;
+let B = 100;
+let C = A / B;
+document.write(C + "<br>");
+
+// Modulus Operator
+let d = 5;
+let e = 2;
+let f = d % e;
+document.write(f + "<br>");
+
+// Increment Operator
+let D = 110;
+D++
+let E = D;
+document.write(E + "<br>");
+
+// Decrement Operator
+let g = 100;
+g--
+let h = g;
+document.write(h + "<br>");
+
+// Exponentiation Operator
+let G = 5;
+let H = G ** 3;
+document.write(H);
