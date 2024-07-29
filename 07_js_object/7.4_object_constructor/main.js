3e03092eedf3635f876f29a7d37bd003c319be79
Rasel Rana Earshad <charry.br10@gmail.com>
2024-07-30 03:41:02 +0600

    js Object constructors

--- 07_js_object/7.4_object_constructor/main.js
+++ 07_js_object/7.4_object_constructor/main.js
@@ -3,10 +3,14 @@ function Person(first,last,age) {
   this.firstName = first;
   this.lastName = last;
   this.age = age;
+  // Adding a Method to a Constructor
+  this.fullName = function () {
+    return this.firstName + " " + this.lastName;
+  };
 }
 // Create three Person objects
 const myFather = new Person('Anower', 'Hossain', 50);
 const myMother = new Person('Rotna', 'Akter', 35);
 const mySister = new Person('Lamiya', 'Akter', '05');
 
-// 
+document.write("My father's name is" + " " + myFather.fullName());
