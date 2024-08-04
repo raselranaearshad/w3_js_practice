d1ab3f4bef5e7acca04f36771f2a7d71cd4c9249
Rasel Rana Earshad <charry.br10@gmail.com>
2024-08-05 02:47:50 +0600

    Added a folder called 13_js_number_properties

---
+++ 13_js_number_properties/README.md
@@ -0,0 +1,14 @@
+# JavaScript Number Properties
+	
+|Property|Description|
+|--------|-----------|
+|EPSILON|The difference between 1 and the smallest number > 1.|
+|MAX_VALUE|The largest number possible in JavaScript
+|
+|MIN_VALUE	|The smallest number possible in JavaScript
+|
+|MAX_SAFE_INTEGER	|The maximum safe integer (253 - 1)|
+|MIN_SAFE_INTEGER|The minimum safe integer -(253 - 1)|
+|POSITIVE_INFINITY|Infinity (returned on overflow)|
+|NEGATIVE_INFINITY|Negative infinity (returned on overflow)|
+
---
+++ 13_js_number_properties/main.js
@@ -0,0 +1,27 @@
+// JavaScript EPSILON
+let epsNum = Number.EPSILON;
+document.write(epsNum + "<br>");
+
+// JavaScript MAX_VALUE
+let maxNum = Number.MAX_VALUE;
+document.write(maxNum + "<br>");
+
+// JavaScript MIN_VALUE
+let minNum = Number.MIN_VALUE;
+document.write(minNum + "<br>");
+
+// JavaScript MAX_SAFE_INTEGER
+let maxSafe = Number.MAX_SAFE_INTEGER;
+document.write(maxSafe + "<br>");
+
+// JavaScript MIN_SAFE_INTEGER
+let minSafe = Number.MIN_SAFE_INTEGER;
+document.write(minSafe + "<br>");
+
+// JavaScript POSITIVE_INFINITY
+let posInf = 1 / 0;
+document.write(posInf + "<br>");
+
+// JavaScript NEGATIVE_INFINITY 
+let negInf = -1 / 0;
+document.write(negInf + "<br>");
